71893ed1391266f176c95ef08605ddc8d29c5f21
Serhan Khan <serhankhan0@gmail.com>
2025-05-28 09:14:01 +0300

    first commit

---
+++ MBStockApp/MBStockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBStockAppApp.swift
+//  MBStockApp
+//
+//  Created by Serhan Khan on 28/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MBStockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
